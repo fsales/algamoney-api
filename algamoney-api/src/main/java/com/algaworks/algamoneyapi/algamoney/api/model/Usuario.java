99d8dfc5c8f5aa57b9399487baa489adcf2b6997
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-07-11 16:21:39 -0300

    configurando usuário no banco

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/model/Usuario.java
@@ -0,0 +1,33 @@
+package com.algaworks.algamoneyapi.algamoney.api.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "usuario")
+
+@Getter
+@Setter
+@ToString(of = {"codigo", "nome"})
+@EqualsAndHashCode(of = {"codigo"})
+public class Usuario implements Serializable {
+
+    @Id
+    private Long codigo;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuario_permissao", joinColumns = @JoinColumn(name = "codigo_usuario")
+            , inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
+    private List<Permissao> permissoes;
+
+}
---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/service/UsuarioService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algamoneyapi.algamoney.api.service;
+
+import com.algaworks.algamoneyapi.algamoney.api.model.Usuario;
+import com.algaworks.algamoneyapi.algamoney.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> findByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+}
