f7a097a7b730026f51d233a8f17d79737908a073
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-07-11 15:01:10 -0300

    configurando refresh_token no cookie

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/config/AuthorizationServerConfig.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/config/AuthorizationServerConfig.java
@@ -26,14 +26,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret("@angul@r0")
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(500)
+                .refreshTokenValiditySeconds(3600*24);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore())
                 .accessTokenConverter(accessTonkenConverter())
+                .reuseRefreshTokens(Boolean.FALSE)
                 .authenticationManager(authenticationManager);
     }
 
---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/toke/RefreshTokenPostProcessor.java
@@ -0,0 +1,56 @@
+package com.algaworks.algamoneyapi.algamoney.api.toke;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+
+        return methodParameter.getMember().getName().equals("postAccessToken");
+    }
+
+    @Override
+    public OAuth2AccessToken beforeBodyWrite(OAuth2AccessToken oAuth2AccessToken, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+        HttpServletRequest request = ((ServletServerHttpRequest) serverHttpRequest).getServletRequest();
+        HttpServletResponse response = ((ServletServerHttpResponse) serverHttpResponse).getServletResponse();
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) oAuth2AccessToken;
+
+        String refreshToken = oAuth2AccessToken.getRefreshToken().getValue();
+        adicinarRefreshTokenCookie(refreshToken, request, response);
+
+        removeRefreshTokenBody(token);
+
+        return oAuth2AccessToken;
+    }
+
+    private void removeRefreshTokenBody(DefaultOAuth2AccessToken token) {
+        token.setRefreshToken(null);
+    }
+
+    private void adicinarRefreshTokenCookie(String refreshToken, HttpServletRequest request, HttpServletResponse response) {
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(Boolean.TRUE);
+        refreshTokenCookie.setSecure(Boolean.FALSE);
+        refreshTokenCookie.setPath(request.getContextPath() + "/oauth/token");
+        refreshTokenCookie.setMaxAge(2595000);
+        response.addCookie(refreshTokenCookie);
+    }
+}
