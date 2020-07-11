2f7031bb38aada6af1a3e80c06ed1f7eda16bfa4
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-07-11 17:06:34 -0300

    removendo refresh_token do cookie

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoney/api/resource/TokenResource.java
@@ -0,0 +1,28 @@
+package com.algaworks.algamoneyapi.algamoney.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/tokens")
+public class TokenResource {
+
+    @DeleteMapping("/revoke")
+    public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); // TODO: Em producao sera true
+        cookie.setPath(req.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        resp.addCookie(cookie);
+        resp.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+
+}
