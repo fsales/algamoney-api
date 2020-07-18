b797a8488e663f3be337452feca7b1b5a5f2185b
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-07-18 19:11:21 -0300

    configurando profile de segurança

--- src/main/java/com/algaworks/algamoneyapi/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoneyapi/algamoney/api/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import com.algaworks.algamoneyapi.algamoney.api.config.token.CustomTokenEnhancer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -17,6 +18,7 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
 
+@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
