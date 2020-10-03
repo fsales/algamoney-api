9868c1e076b9671a3f785e26b74d8493ea6bb93f
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-10-03 19:58:46 -0300

    Usando property binding

--- angular/projeto-teste/src/app/app.component.ts
+++ angular/projeto-teste/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'projeto-teste';
 
-  nome = 'teste';
+  nome = '';
 
   adicinar(nome: string){
     this.nome = nome;
