37a9277021b4c012350a3085a88b2de7150acbf8
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-10-03 19:46:02 -0300

    usando event binding

--- angular/projeto-teste/src/app/app.component.ts
+++ angular/projeto-teste/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-teste';
+
+  nome = 'teste';
+
+  adicinar(){
+    console.log(`Adicionando ${this.nome}`);
+  }
+
+  alterarNome(event: any){
+    this.nome = event.target.value;
+  }
 }
